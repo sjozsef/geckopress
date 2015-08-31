b9ea291c1a991b45ca8ffefba17f6158ac4eaf53
József Samu <sjozsef0227@gmail.com>
2015-08-31 19:20:13 +0000

    Slider

--- parts/header.php
+++ parts/header.php
@@ -1,3 +1,11 @@
+<?php defined('ABSPATH') or die("Direct access not allowed."); ?>
+
 <?php
 get_template_part('snippets/header/header-navigation');
+
+if(is_home() || is_front_page())
+{
+    get_template_part('snippets/header/header-home-slider');
+}
+
 ?>
