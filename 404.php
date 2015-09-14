5210ff0b5304c566902b02e1863fde0c6c6656a2
József Samu <sjozsef0227@gmail.com>
2015-09-14 02:58:38 +0000

    little code quality improvements

--- 404.php
+++ 404.php
@@ -1,11 +1,22 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Displaying a basic 404 page
+ *
+ * This file is loaded by WordPres if the content does not exists. Message is translateable with gettext.
+ *
+ * @package GeckoPress
+ * @subpackage Main
+ * @since 0.1
+ */
+
+get_header(); ?>
 
 <div class="container">
             
     <div class="main-content not-found-content">
     
     <h1>404</h1>
-    <p><?php _e("It seems that the page you were trying to reach couldn't be found, or maybe you have mis-typed the URL, please check your spelling. We think that the best thing to do is to use the search bar at the upper right of this page, or check out our homepage. Please contact us if the problem persists or if you definitely can't find what you are looking for. Thank you very much."); ?></p>
+    <p><?php esc_attr_e( "It seems that the page you were trying to reach couldn't be found, or maybe you have mis-typed the URL, please check your spelling. We think that the best thing to do is to use the search bar at the upper right of this page, or check out our homepage. Please contact us if the problem persists or if you definitely can't find what you are looking for. Thank you very much." ); ?></p>
     
     </div>
     
--- functions.php
+++ functions.php
@@ -5,15 +5,14 @@ require_once( TEMPLATEPATH . '/functions/utils.php' );
 require_once( TEMPLATEPATH . '/functions/config.php' );
 require_once( TEMPLATEPATH . '/functions/scripts.php' );
 require_once( TEMPLATEPATH . '/functions/styles.php' );
-require_once( TEMPLATEPATH . '/functions/options.php' ); 
+require_once( TEMPLATEPATH . '/functions/options.php' );
 require_once( TEMPLATEPATH . '/functions/nav-menus.php' );
 require_once( TEMPLATEPATH . '/functions/sidebars.php' );
 require_once( TEMPLATEPATH . '/functions/image-sizes.php' );
 require_once( TEMPLATEPATH . '/functions/theme-supports.php' );
 
-if(!is_admin())
-{
-    require_once( TEMPLATEPATH . '/functions/wp_bootstrap_pagination.php');
+if ( ! is_admin() ) {
+    require_once( TEMPLATEPATH . '/functions/wp_bootstrap_pagination.php' );
 }
 
 ?>
